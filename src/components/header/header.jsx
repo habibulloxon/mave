90280b7d97e5ae37432cbcd87eb6d817f0e262c5
khabibullokhon <k01346595@gmail.com>
2023-07-11 13:54:06 +0500

    assets added

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import "./header.css"
+
+const Header = () => {
+    return (
+        <header>
+            <a href="#" className="header__logo">mave</a>
+        </header>
+    )
+}
+
+export default Header
